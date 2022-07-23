afa14d99e59f29d2a7ad9ae591e1437e5e53e452
spekboom <69419485+luke-da-nuke@users.noreply.github.com>
2022-07-23 20:56:57 +0200

    all slider now works

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request,  url_for, redirect
 app = Flask(__name__)                                               #creates the flask webapp
 app.secret_key = "some highway in america or smth"                  #for secure transfer
 brights = {                                                         #dictionary that stores brightness value of each light section from 0-255
+    "AllRng" : None,                                                #all only needs to be used for remembering the value on the webpage, not for changing the lights as the other values are automatically updated by Ruben's js
     "GallaryRng" : None,
     "PublicFRng" : None,
     "PublicBRng" : None,
@@ -10,9 +11,9 @@ brights = {                                                         #dictionary 
 c=20
 @app.route("/")                                                      #creates main page at ./ and names it home
 def home():
-    return render_template('index.html',v=brights)
+    return render_template('index.html',v=brights)                   #renders index.html and parses the dictionary to be used in the {{}} things
 @app.route("/slider_update", methods=["POST"])                       #API to update dictionary when sliders are changed
-def add_compliment():
+def slider_update():
     for x in brights:
         brights[x] = request.values.get(x)
         debugtxt = "the value of {} is {}"
