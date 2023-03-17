dbad931b788899aa9e1a8be0897f9e9cdcd9c996
luke-da-nuke <luke_plastow@fis.edu>
2023-03-17 11:36:18 +0100

    asdf

--- app.py
+++ app.py
@@ -181,3 +181,7 @@ def api(keyIn, brightIn):
     else:
         print ("invalid")
         return "invalid:   " + keyIn + str(brightIn)
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000, host='0.0.0.0')
